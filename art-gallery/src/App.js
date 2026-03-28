bac15c2f19da2f969cec4a507a7bd45cc6341e25
Mhirax <olowookeremiracle14@gmail.com>
2026-03-28 06:38:04 +0100

    adding parent components

--- art-gallery/src/App.js
+++ art-gallery/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import NewsSection from "./components/NewsSection";
+import MapSection from "./components/MapSection";
+import ArtGallery from "./components/ArtGallery";
+import data from "./data/artworks.json";
+import "./App.scss";
 
 function App() {
+  const { artworks, announcements } = data;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Navbar />
+      <Hero />
+      <NewsSection announcements={announcements} />
+      <ArtGallery artworks={artworks} />
+      <MapSection />
+
+      <footer className="footer">
+        <div className="container">
+          <p>&copy; 2024 ArtGallery. All rights reserved.</p>
+          <small>
+            Discover extraordinary art from masters and emerging talents in
+            Nigeria and beyond
+          </small>
+        </div>
+      </footer>
     </div>
   );
 }
