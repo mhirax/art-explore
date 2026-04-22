7d480aaabac5dc080e4bbb4fd4b3cebef47b3a78
Mhirax <olowookeremiracle14@gmail.com>
2026-04-22 09:54:44 +0100

    feat: create galleries data file with Nike Art Gallery

---
+++ Art-Explore Frontend/src/data/MapViewData.js
@@ -0,0 +1,10 @@
+export const galleries = [
+  {
+    id: 1,
+    name: "Nike Art Gallery",
+    address: "2 Elegushi Rd, Lekki Phase I, Lagos",
+    lng: 3.4479,
+    lat: 6.4563,
+    region: "Island",
+  },
+];
