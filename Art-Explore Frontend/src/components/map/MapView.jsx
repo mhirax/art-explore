3cb87b3d0b1cff19d4af957135ed067e900b4158
Mhirax <olowookeremiracle14@gmail.com>
2026-04-22 09:40:11 +0100

    feat: add popup with name and address to Nike marker

--- Art-Explore Frontend/src/components/map/MapView.jsx
+++ Art-Explore Frontend/src/components/map/MapView.jsx
@@ -6,21 +6,26 @@ import "./MapView.scss";
 const MapView = () => {
   const mapContainer = useRef(null);
 
-  useEffect(() => {
-    const map = new maplibregl.Map({
-      container: mapContainer.current,
-      style: "https://demotiles.maplibre.org/style.json",
-      center: [3.3792, 6.5244], // Lagos [lng, lat]
-      zoom: 11,
-    });
-      
-       // 👇 ADD NIKE MARKER RIGHT HERE
-  new maplibregl.Marker()
-    .setLngLat([3.4479, 6.4563])
-    .addTo(map);
+ useEffect(() => {
+   const map = new maplibregl.Map({
+     container: mapContainer.current,
+     style: "https://demotiles.maplibre.org/style.json",
+     center: [3.3792, 6.5244],
+     zoom: 11,
+   });
 
-    return () => map.remove();
-  }, []);
+   // 👇 REPLACED THIS SECTION
+   new maplibregl.Marker()
+     .setLngLat([3.4479, 6.4563])
+     .setPopup(
+       new maplibregl.Popup({ offset: 25 }).setHTML(
+         "<h4>Nike Art Gallery</h4><p>2 Elegushi Rd, Lekki Phase I, Lagos</p>",
+       ),
+     )
+     .addTo(map);
+
+   return () => map.remove();
+ }, []);
 
   return <div ref={mapContainer} className="map-container" />;
 };
