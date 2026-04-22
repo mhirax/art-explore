cea10ebe08996d5fd12d53bea4e4e9e2de65b40d
Mhirax <olowookeremiracle14@gmail.com>
2026-04-22 09:30:01 +0100

    initialize MapLibre map centered on Lagos with react hooks

--- Art-Explore Frontend/src/components/map/MapView.jsx
+++ Art-Explore Frontend/src/components/map/MapView.jsx
@@ -1,9 +1,23 @@
+import { useEffect, useRef } from "react";
 import maplibregl from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 import "./MapView.scss";
 
 const MapView = () => {
-  return <div className="map-container" />;
+  const mapContainer = useRef(null);
+
+  useEffect(() => {
+    const map = new maplibregl.Map({
+      container: mapContainer.current,
+      style: "https://demotiles.maplibre.org/style.json",
+      center: [3.3792, 6.5244], // Lagos [lng, lat]
+      zoom: 11,
+    });
+
+    return () => map.remove();
+  }, []);
+
+  return <div ref={mapContainer} className="map-container" />;
 };
 
 export default MapView;
