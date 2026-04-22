a3c7a40e38d221a476d391c21e43bd543b210620
Mhirax <olowookeremiracle14@gmail.com>
2026-04-22 09:20:15 +0100

    create the basic fundamental component

--- Art-Explore Frontend/src/components/map/MapView.jsx
+++ Art-Explore Frontend/src/components/map/MapView.jsx
@@ -1,2 +1,8 @@
 import maplibregl from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
+
+const MapView = () => {
+  return <div className="map-container" />;
+};
+
+export default MapView;
